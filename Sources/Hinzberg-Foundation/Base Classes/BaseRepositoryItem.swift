0bca88a7238498798ff73391d8ba48bdd0ca2519
Holger Hinzberg <holger@hinzberg.de>
2022-10-03 17:23:26 +0200

    2022-10-03 Update

--- Sources/Hinzberg-Foundation/Base Classes/BaseRepositoryItem.swift
+++ Sources/Hinzberg-Foundation/Base Classes/BaseRepositoryItem.swift
@@ -6,6 +6,10 @@ open class BaseRepositoryItem : Identifiable, Equatable, ObservableObject {
     @Published public var id = UUID()
     @Published public var selected : Bool = false
     
+    public init() {
+        
+    }
+    
     public static func == (lhs: BaseRepositoryItem, rhs: BaseRepositoryItem) -> Bool {
         return lhs.id == rhs.id
     }
